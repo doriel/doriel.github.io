a37da07475cada698f1830617cb033d0923583c5
Doriel Fonseca <doriel3572@gmail.com>
2014-05-01 06:55:39 -0700

    why use jquery in this site??
    No need to use jquery yet ;)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-$(document).ready(function() {
-  $("#btn_sobre").click(function(e) {
-  	e.preventDefault();
-    $("#sobre").fadeIn();
-    $(this).hide();
-  });
-});
+(function () {
+	var botao = document.querySelector("#btn_sobre");
+		mostra = document.querySelector("#sobre");
+
+	botao.addEventListener("click", esconde_mostra, false);
+
+	function esconde_mostra () {
+		mostra.style.display = "block";
+		botao.style.display = "none";
+		
+	}
+
+}());
+
