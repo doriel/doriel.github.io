82f30930a9c36bdd66a461fcef984122023839cd
Doriel Fonseca <doriel3572@gmail.com>
2014-05-13 07:23:46 -0700

    Adding same new info to the web page

--- js/script.js
+++ js/script.js
@@ -1,45 +1,7 @@
-window.setTimeout(function() {
-        var descarregador = new Progress.bar({ 
-        	id: "percentagem",
-        	autoRemove: false,
-        	backgroundSpeed: -5,
-        	type: "discharge",
-        	showPercentage: true });
-        descarregador.renderTo(document.getElementById('carregador'));
-
-        var percenta = 100;
-        window.setInterval(function() {
-            percenta = percenta - Math.floor(Math.random()*20);
-            percenta = percenta <= 0 ? 100 : percenta;
-            descarregador.update(percenta);
-
-            if (percenta < 10) {
-            	var elem1 = document.getElementById('para');
-            	para.style.opacity = 0;
-
-
-     function carregando(){
-        var elem1 = document.getElementById('loading'),
-            elem2 = document.getElementById('menu'),
-            primeira = feid(elem1,8),
-            segunda = feid(elem2,18);
-                
-
-        primeira.on();
-        segunda.on();
-            }
-            window.setTimeout(carregando,2);
-
-
-
-        	}
-            
-        }, 500);
-
-        
-    }, 500)
-
-
-
-
-			
+$(document).ready(function() {
+  $("#btn_sobre").click(function(e) {
+  	e.preventDefault();
+    $("#sobre").fadeIn();
+    $(this).hide();
+  });
+});
