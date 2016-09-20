badbf4927cdc6fc621e3f11dc42e646c36432085
Doriel Fonseca <doriel3572@gmail.com>
2016-09-20 12:45:06 +0100

    adding js libs and same routing script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+      templateUrl : 'vista/index.html',
+  })
+
+  .when('/sobre', {
+      templateUrl : 'vista/sobre.html',
+  })
+
+  .when('/projectos', {
+      templateUrl : 'vista/projectos.html',
+  })
+
+  .when('/contacto', {
+      templateUrl : 'vista/contacto.html',
+  });
+});
 
 
